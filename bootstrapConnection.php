81515ad8f7743ca8e127c4029994725e53e3c0e0
Mateusz Naumiuk <mateusznaumiuk952@gmail.com>
2022-11-14 19:08:04 +0100

    14.11.2022 19:08

--- bootstrapConnection.php
+++ bootstrapConnection.php
@@ -10,5 +10,3 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
 <link rel="stylesheet" href="main.css">
 <link rel="shortcut icon" href="img/logo_min.png">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
