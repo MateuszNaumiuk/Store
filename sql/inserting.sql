331d9952ce11219ff569335e1a13ca3fbe912e43
WielkiRage11 <piterrrr@vp.pl>
2022-11-28 20:23:01 +0100

    On branch main

--- sql/inserting.sql
+++ sql/inserting.sql
@@ -19,22 +19,22 @@ insert into category VALUES
 inserting values into pictures
 */
 
-insert into pictures(`picture_id`, `pic_name`, `price`, `description`, `height`, `width`,`image_path`) VALUES
-(1,'picture1',10,'We share the best photos on the internet. Best prices and customer service!', 100,100,'grafiki/1.jpg'),
-(2,'picture2',10,'We share the best photos on the internet. Best prices and customer service!', 200,200,'grafiki/2.jpg'),
-(3,'picture3',10,'We share the best photos on the internet. Best prices and customer service!', 600,600,'grafiki/3.jpg'),
-(4,'picture4',10,'We share the best photos on the internet. Best prices and customer service!', 600,600,'grafiki/4.jpg'),
-(5,'picture5',10,'We share the best photos on the internet. Best prices and customer service!', 600,600,'grafiki/5.jpg'),
-(6,'picture6',10,'We share the best photos on the internet. Best prices and customer service!', 600,600,'grafiki/6.jpg'),
-(7,'picture7',10,'We share the best photos on the internet. Best prices and customer service!', 600,600,'grafiki/7.jpg'),
-(8,'picture8',10,'We share the best photos on the internet. Best prices and customer service!', 600,600,'grafiki/8.jpg'),
-(9,'picture9',10,'We share the best photos on the internet. Best prices and customer service!', 600,600,'grafiki/9.jpg'),
-(10,'picture10',10,'We share the best photos on the internet. Best prices and customer service!', 600,600,'grafiki/10.jpg'),
-(11,'picture11',10,'We share the best photos on the internet. Best prices and customer service!', 600,600,'grafiki/11.jpg'),
-(12,'picture12',10,'We share the best photos on the internet. Best prices and customer service!', 600,600,'grafiki/12.jpg'),
-(13,'picture13',10,'We share the best photos on the internet. Best prices and customer service!', 600,600,'grafiki/13.jpg'),
-(14,'picture14',10,'We share the best photos on the internet. Best prices and customer service!', 600,600,'grafiki/14.jpg'),
-(15,'picture15',10,'We share the best photos on the internet. Best prices and customer service!', 600,600,'grafiki/15.jpg');
+insert into pictures(`picture_id`, `pic_name`, `price`, `description`, `height`, `width`,`image_path`,`privileges`) VALUES
+(1,'picture1',10,'We share the best photos on the internet. Best prices and customer service!', 100,100,'grafiki/1.jpg','user'),
+(2,'picture2',10,'We share the best photos on the internet. Best prices and customer service!', 200,200,'grafiki/2.jpg','admin'),
+(3,'picture3',10,'We share the best photos on the internet. Best prices and customer service!', 600,600,'grafiki/3.jpg','user'),
+(4,'picture4',10,'We share the best photos on the internet. Best prices and customer service!', 600,600,'grafiki/4.jpg','admin'),
+(5,'picture5',10,'We share the best photos on the internet. Best prices and customer service!', 600,600,'grafiki/5.jpg','user'),
+(6,'picture6',10,'We share the best photos on the internet. Best prices and customer service!', 600,600,'grafiki/6.jpg','admin'),
+(7,'picture7',10,'We share the best photos on the internet. Best prices and customer service!', 600,600,'grafiki/7.jpg','user'),
+(8,'picture8',10,'We share the best photos on the internet. Best prices and customer service!', 600,600,'grafiki/8.jpg','admin'),
+(9,'picture9',10,'We share the best photos on the internet. Best prices and customer service!', 600,600,'grafiki/9.jpg','user'),
+(10,'picture10',10,'We share the best photos on the internet. Best prices and customer service!', 600,600,'grafiki/10.jpg','user'),
+(11,'picture11',10,'We share the best photos on the internet. Best prices and customer service!', 600,600,'grafiki/11.jpg','user'),
+(12,'picture12',10,'We share the best photos on the internet. Best prices and customer service!', 600,600,'grafiki/12.jpg','admin'),
+(13,'picture13',10,'We share the best photos on the internet. Best prices and customer service!', 600,600,'grafiki/13.jpg','user'),
+(14,'picture14',10,'We share the best photos on the internet. Best prices and customer service!', 600,600,'grafiki/14.jpg','user'),
+(15,'picture15',10,'We share the best photos on the internet. Best prices and customer service!', 600,600,'grafiki/15.jpg','admin');
 
 
 
