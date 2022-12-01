3aae1b055d49959775abc6fca6b275fb6068dc44
Mateusz Naumiuk <mateusznaumiuk952@gmail.com>
2022-12-01 14:03:10 +0100

    On branch main

--- zarzadzanie.php
+++ zarzadzanie.php
@@ -20,11 +20,11 @@
                         <div class="accordion" id="accordionExample">
                             <div class="accordion-item">
                                 <h2 class="accordion-header" id="headingOne">
-                                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
                                         Zamowienia
                                     </button>
                                 </h2>
-                                <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                                <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
                                     <div class="accordion-body">
                                         <button type="submit" name="zamowienia" value="zamowienia" class="btn btn-secondary my-4">Zamowienia</button><br>
                                     </div>
